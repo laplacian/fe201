2d4024fa112f7415edbf494002c341afdc594f1b
unknown <USER@.(none)>
2011-07-06 17:00:21 +0300

    [tweetmodel] twitter.tweetmodel is added as a provide and twitter.tweet is added as a require. function types are changed.

--- twitter/js/models/ProfileModel.js
+++ twitter/js/models/ProfileModel.js
@@ -2,3 +2,37 @@
 // Techinox Commercial License
 //
 // @author Armagan Amcalar <armagan@tart.com.tr>
+
+goog.provide('twitter.ProfileMode');
+goog.require('twitter.Profie');
+
+twitter.ProfileModel = function() {
+
+}
+
+twitter.ProfileModel.prototype.createProfile = function (bio,photoUrl,location) {
+        var profile = new twitter.Profile(bio,photoUrl,location) ;
+        localStorage.setItem(profile.id,profile);
+};
+
+twitter.ProfileModel.prototype.getProfileByUserId = function (userId) {
+    for(var profile in localStorage) {
+        profile = localStorage.getObject(profile);
+        if(profile.userId==userId) {
+            return profile;
+        }
+    }
+    return false;
+};
+twitter.ProfileModel.prototype.editProfileByUserId = function (userId,bio,photoUrl,location) {
+        for(var profile in localStorage) {
+            profile = localStorage.getObject(profile);
+            if(profile.userId==userId) {
+             profile.bio = bio;
+             profile.photoUrl = photoUrl;
+             profile.location = location;
+             localStorage.setObject(profile.id,profile);
+
+        }
+        }
+};
