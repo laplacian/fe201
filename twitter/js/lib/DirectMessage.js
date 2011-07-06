5b2d5add2f5f4ec4070677626e3e397747e131e5
unknown <USER@.(none)>
2011-07-06 16:46:28 +0300

    [tart] DirectMessage is added as a provide, tart is added as a require.

--- twitter/js/lib/DirectMessage.js
+++ twitter/js/lib/DirectMessage.js
@@ -5,9 +5,12 @@
  * Time: 11:22
  * To change this template use File | Settings | File Templates.
  */
-var DirectMessage = function(from , to, body){
+
+goog.provide('twitter.DirectMessage');
+goog.require('tart');
+twitter.DirectMessage = function(from , to, body){
     this.id = tart.getUId();
     this.from = from;
     this.to = to;
     this.body = body;
-}
+    }
