18d667713b0cf0f8c3af6d47f747a0afc19837df
unknown <USER@.(none)>
2011-07-06 16:51:13 +0300

    [profile] twitter.profile is added as a provide and tart is added as a require.

--- twitter/js/lib/Profile.js
+++ twitter/js/lib/Profile.js
@@ -5,7 +5,11 @@
  * Time: 11:36
  * To change this template use File | Settings | File Templates.
  */
-var Profile = function(userId, bio, photoUrl, location){
+
+goog.provide('twitter.Profile');
+goog.require('tart');
+
+twitter.Profile = function(userId, bio, photoUrl, location){
     this.id = tart.getUId();
     this.userId = userId;
     this.bio = bio;
