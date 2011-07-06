ce5c48e3a556cc78add5a5cab88a33c9ec75050e
unknown <USER@.(none)>
2011-07-06 16:54:23 +0300

    [followmanager] twitter.session is added as a provide and twitter.follow and twitter.usermodel is added as a require. function types are changed.

--- twitter/js/models/FollowManager.js
+++ twitter/js/models/FollowManager.js
@@ -3,11 +3,53 @@
 //
 // @author Armagan Amcalar <armagan@tart.com.tr>
 
-var FollowManager = function() {
+goog.provide('twitter.FollowManager');
+goog.require('twitter.Follow');
+goog.require('twitter.UserModel');
 
+twitter.FollowManager = function() {
+
+}
+
+twitter.FollowManager.prototype.createFollow = function (follower,following) {
+        var follow = new twitter.Follow(follower.id,following.id);
+        localStorage.setObject(follow.id, follow);
+};
+/**
+ *
+ * @param {number} userId A user id.
+ * @return(Array.<Users>} Followa of the given user.
+ */
+
+twitter.FollowManager.prototype.getFollowingByUserId = function (userId) {
+    var array = [];
+    for(var following in localStorage) {
+        following = localStorage.getObject(following);
+        if(following && following.userId && following.userId==userId) {
+            array.push(following)
+        }
+    }
+    return array;
 };
 
-FollowManager.prototype.createFollow = function(follower, following) {
-    var follow = new Follow(follower.id, following.id);
-    localStorage.setObject(follow.id, follow);
-}
+/**
+ *
+ * @param userId
+ * @return {(boolean|number)}
+ */
+FollowManager.prototype.getFollowersByUserId = function (userId) {
+    //var returnValue= false;
+    //if(userId == "1") {
+      //  returnValue= "@2"
+    //}
+    //return returnValue;
+
+    var array = [];
+    for(var follower in localStorage) {
+        follower = localStorage.getObject(follower);
+        if(follower && follower.userId && follower.userId==userId) {
+            array.push(follower)
+        }
+    }
+    return array;
+};
