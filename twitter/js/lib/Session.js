8d14403eaaf82cf52a7c31f355c4aef67e8d0018
unknown <USER@.(none)>
2011-07-06 16:50:36 +0300

    [session] twitter.session is added as a provide and tart is added as a require.

--- twitter/js/lib/Session.js
+++ twitter/js/lib/Session.js
@@ -5,7 +5,10 @@
  * Time: 11:37
  * To change this template use File | Settings | File Templates.
  */
-var Session = function (userId) {
+
+goog.provide('twitter.Session');
+goog.require('tart');
+twitter.Session = function (userId) {
    this.id = tart.getUId();
    this.userId = userId;
  };
